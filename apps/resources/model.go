261167ae92290d2eb551833ce14cac0c2c0a6e43
yhkl-dev <kaiyang939325@gmail.com>
2021-01-16 21:45:32 +0800

    update

--- apps/resources/model.go
+++ apps/resources/model.go
@@ -4,21 +4,24 @@ import (
 	"dbdms/db"
 )
 
-// Resources role struct
+// Resource resource struct
 type Resource struct {
-	ResourceID           int    `gorm:"column:resource_id;primary_key" json:"resource_id" form:"resource_id"`
-	ResourceName         string `gorm:"type:varchar(32);column:resource_name;unique;not null" json:"resource_name" form:"resource_name" binding:"required"`
-	ResourceHostIP       string `gorm:"column:resource_host_ip" json:"resource_host_ip" form:"resource_host_ip" binding:"required"`
-	ResourceUser         string `gorm:"type:varchar(32);column:resource_user" json:"resource_user" form:"resource_user" binding:"required"`
-	ResourcePassword     string `gorm:"type:varchar(64);column:resource_password" json:"resource_password" form:"resource_password" binding:"required"`
-	ResourcePort         int    `gorm:"column:resource_port" json:"resource_port" form:"resource_port" binding:"required"`
-	ResourcePassSalt     string `gorm:"type:varchar(32);column:resource_pass_salt" json:"resource_pass_salt" form:"resource_pass_salt"`
-	ResourceDatabaseName string `gorm:"type:varchar(32);column:resource_database_name" json:"resource_database_name" form:"resource_database_name"`
-	ResourceDescription  string `gorm:"type:varchar(32);column:resource_description" json:"resource_description" form:"resource_description" binding:"required"`
-	ResourceTypeID       int
-	ResourceType         ResourceType `gorm:"foreignKey:ResourceTypeID" json:"resource_type" form:"resource_port" binding:"required"`
+	ResourceID               int    `gorm:"column:resource_id;primary_key" json:"resource_id" form:"resource_id"`
+	ResourceName             string `gorm:"type:varchar(32);column:resource_name;unique;not null" json:"resource_name" form:"resource_name" binding:"required"`
+	ResourceHostIP           string `gorm:"column:resource_host_ip" json:"resource_host_ip" form:"resource_host_ip" binding:"required"`
+	ResourceUser             string `gorm:"type:varchar(32);column:resource_user" json:"resource_user" form:"resource_user" binding:"required"`
+	ResourcePassword         string `gorm:"type:varchar(64);column:resource_password" json:"resource_password" form:"resource_password"`
+	ResourcePublicKeyName    string `gorm:"type:varchar(500);column:resource_public_key_name" json:"resource_public_key_name" form:"resource_public_key_name"`
+	ResourcePublicKeyContent string `gorm:"type:varchar(500);column:resource_public_key_content" json:"resource_public_key_content" form:"resource_public_key_content"`
+	ResourcePort             int    `gorm:"column:resource_port" json:"resource_port" form:"resource_port" binding:"required"`
+	ResourcePassSalt         string `gorm:"type:varchar(32);column:resource_pass_salt" json:"resource_pass_salt" form:"resource_pass_salt"`
+	ResourceDatabaseName     string `gorm:"type:varchar(32);column:resource_database_name" json:"resource_database_name" form:"resource_database_name"`
+	ResourceDescription      string `gorm:"type:varchar(32);column:resource_description" json:"resource_description" form:"resource_description" binding:"required"`
+	ResourceTypeID           int
+	ResourceType             ResourceType `gorm:"foreignKey:ResourceTypeID" json:"resource_type" form:"resource_port" binding:"required"`
 }
 
+// ResourceType resource type struct
 type ResourceType struct {
 	ResourceTypeID          int    `gorm:"column:resource_type_id;primary_key" json:"resource_type_id" form:"resource_type_id"`
 	ResourceParentTypeID    int    `gorm:"column:resource_parent_id;default:0" json:"resource_parent_id" form:"resource_parent_id"`
